eba0263e3f0866d14293884406b5dca75827979d
MohammadZeineddine <99921709+MohammadZeineddine@users.noreply.github.com>
2024-12-18 19:51:48 +0200

    Add entrypoint script for training and inference processes

---
+++ telco_churn/src/telco_churn/scripts/entrypoint.py
@@ -0,0 +1,27 @@
+import os
+import sys
+from loguru import logger
+
+
+def train():
+    logger.info("Starting training process...")
+    os.system(
+        f"python src/telco_churn/scripts/train_batch.py --config {os.getenv('CONFIG_PATH')}")
+
+
+def inference():
+    logger.info("Starting inference process...")
+    os.system(
+        f"python src/telco_churn/scripts/inference_batch.py "
+        f"--config {os.getenv('CONFIG_PATH')} --data {os.getenv('DATA_PATH')}"
+    )
+
+
+if __name__ == "__main__":
+    task = sys.argv[1] if len(sys.argv) > 1 else "help"
+    if task == "train":
+        train()
+    elif task == "inference":
+        inference()
+    else:
+        print("Usage: docker run <image_name> [train|inference]")
